e9a82c22798e94bacbf7b7170c57c4e87d7c9b44
Mehran Prs <mehr.prs@gmail.com>
2020-02-25 11:32:57 +0330

    Update HTTPErrorHandler to handle even if kitty context does not exists.

--- error.go
+++ error.go
@@ -7,29 +7,41 @@ import (
 
 // HTTPErrorHandler is the echo error handler.
 // this function need to the KittyContext middleware.
-func HTTPErrorHandler(requestErr error, c echo.Context) {
-	if _, ok := requestErr.(kitty.Reply); !ok {
-		requestErr = errUnknownError.SetInternalMessage(requestErr.Error())
+func HTTPErrorHandler(l kitty.Logger, t kitty.Translator) echo.HTTPErrorHandler {
+	return func(requestErr error, c echo.Context) {
+		l := l
+		t := t
+
+		if _, ok := requestErr.(kitty.Reply); !ok {
+			requestErr = errUnknownError.SetInternalMessage(requestErr.Error())
+		}
+
+		kerr := requestErr.(kitty.Error)
+
+		kittyCtx := c.Get(ContextKeyKittyCtx).(kitty.Context)
+
+		// Maybe error occur before set kitty context in middleware
+		if kittyCtx != nil {
+			l = kittyCtx.Logger()
+			t = kittyCtx.Translator()
+		}
+
+		msg, err := t.Translate(kerr.Key(), kerr.Params())
+
+		if err != nil {
+			msg = ""
+		}
+
+		requestErr = c.JSON(kerr.HTTPStatus(), kitty.NewBody(kerr.Code(), msg, kitty.Data(kerr.Data())))
+
+		// Report if need to report:
+		if kerr.ShouldReport() {
+			kerr.Report(l, t)
+		}
+
+		if requestErr != nil {
+			l.Error(requestErr)
+		}
 	}
 
-	kerr := requestErr.(kitty.Error)
-
-	kittyCtx := c.Get(ContextKeyKittyCtx).(kitty.Context)
-	translator := kittyCtx.Translator()
-	msg, err := translator.Translate(kerr.Key(), kerr.Params())
-
-	if err != nil {
-		msg = ""
-	}
-
-	requestErr = c.JSON(kerr.HTTPStatus(), kitty.NewBody(kerr.Code(), msg, kitty.Data(kerr.Data())))
-
-	// Report if need to report:
-	if kerr.ShouldReport() {
-		kerr.Report(kittyCtx.Logger(), kittyCtx.Translator())
-	}
-
-	if requestErr != nil {
-		kittyCtx.Logger().Error(requestErr)
-	}
 }
