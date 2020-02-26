52afaa5b819d4c701c71e8c02cd2ac4f71310f87
Mehran Prs <mehr.prs@gmail.com>
2020-02-26 11:19:02 +0330

    Update DebugMode middleware

--- debug.go
+++ debug.go
@@ -5,7 +5,7 @@ import (
 )
 
 // KittyContext set kitty context on each request.
-func DebugMode(e *echo.Echo) echo.MiddlewareFunc {
+func DebugMiddleware(e *echo.Echo) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(ctx echo.Context) error {
 			if !e.Debug {
