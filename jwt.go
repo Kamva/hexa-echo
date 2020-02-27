a57ed18d30b64d0da01f1a5b8ca2a9fcedaca526
Mehran Prs <mehr.prs@gmail.com>
2020-02-27 22:28:06 +0330

    Update error handler

--- jwt.go
+++ jwt.go
@@ -58,7 +58,7 @@ func jwtErrorHandler(err error) error {
 	}
 
 	// otherwise authorization error
-	return errInvalidOrExpiredJwt.SetInternalMessage(err.Error())
+	return errInvalidOrExpiredJwt.SetError(err)
 }
 
 var jwtConfig = middleware.JWTConfig{
