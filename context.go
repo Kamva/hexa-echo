4fb14c0343a734e4baeb3e6f5e9cbb3ea57cd216
Mehran Prs <mehr.prs@gmail.com>
2020-02-28 17:24:48 +0330

    Update to the new version of kity

--- context.go
+++ context.go
@@ -61,36 +61,12 @@ func getCorrelationID(ctx echo.Context) (string, kitty.Error) {
 	return cid, nil
 }
 
-// tuneLogger function tune the logger for users request.
-func tuneLogger(ctx echo.Context, requestID string, correlationID string, u kitty.User, logger kitty.Logger) kitty.Logger {
-
-	logger = logger.WithFields(
-		"__guest__", u.IsGuest(),
-		"__user_id__", u.GetID(),
-		"__username__", u.GetUsername(),
-		"__request_id__", requestID,
-		"__correlation_id__", correlationID,
-	)
-
-	return logger
-}
-
-// localizeTranslator localize translator for each request relative to the request headers.
-func localizeTranslator(ctx echo.Context, t kitty.Translator) kitty.Translator {
-	req := ctx.Request()
-	// Get Request ID if exists:
-	al := req.Header.Get("Accept-Language")
-	if al != "" {
-		return t.Localize(al)
-	}
-
-	return t.Localize()
-}
-
 // KittyContext set kitty context on each request.
 func KittyContext(logger kitty.Logger, translator kitty.Translator) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(ctx echo.Context) error {
+			req := ctx.Request()
+
 			user, err := getKittyUser(ctx)
 
 			if err != nil {
@@ -109,12 +85,10 @@ func KittyContext(logger kitty.Logger, translator kitty.Translator) echo.Middlew
 				return tracer.Trace(err)
 			}
 
-			logger := tuneLogger(ctx, rid, cid, user, logger)
-
-			translator := localizeTranslator(ctx, translator)
+			al := req.Header.Get("Accept-Language")
 
 			// Set context
-			ctx.Set(ContextKeyKittyCtx, kitty.NewCtx(rid, cid, user, logger, translator))
+			ctx.Set(ContextKeyKittyCtx, kitty.NewCtx(rid, cid, al, user, logger, translator))
 
 			// Set context logger
 			ctx.SetLogger(KittyLoggerToEchoLogger(logger))
