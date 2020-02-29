562158e1e2d12d539227ce5e63b88ed4117f7477
Mehran Prs <mehr.prs@gmail.com>
2020-02-29 23:35:46 +0330

    Chagne error codes

--- errors.go
+++ errors.go
@@ -16,49 +16,49 @@ var (
 	//--------------------------------
 	// User and authentication Errors
 	//--------------------------------
-	errUserNotFound = kitty.NewError(http.StatusInternalServerError, "ke.1.e.0",
+	errUserNotFound = kitty.NewError(http.StatusInternalServerError, "ke.us.e.0",
 		kitty.ErrKeyInternalError, errors.New("user not found"),
 	)
 
-	errContextUserNotImplementedKittyUser = kitty.NewError(http.StatusInternalServerError, "ke.1.e.1",
+	errContextUserNotImplementedKittyUser = kitty.NewError(http.StatusInternalServerError, "ke.us.e.1",
 		kitty.ErrKeyInternalError, errors.New("context's user not implemented the kitty User interface"),
 	)
 
-	errJwtMissing = kitty.NewError(http.StatusBadRequest, "ke.1.e.2",
-		"missing_jwt_token", errors.New("missing or malformed jwt"))
+	errJwtMissing = kitty.NewError(http.StatusBadRequest, "ke.us.e.2",
+		"err_missing_jwt_token", errors.New("missing or malformed jwt"))
 
-	errInvalidOrExpiredJwt = kitty.NewError(http.StatusUnauthorized, "ke.1.e.3",
-		"invalid_or_expired_jwt", errors.New("invalid or expired jwt"))
+	errInvalidOrExpiredJwt = kitty.NewError(http.StatusUnauthorized, "ke.us.e.3",
+		"err_invalid_or_expired_jwt", errors.New("invalid or expired jwt"))
 
-	errUserMustBeGuest = kitty.NewError(http.StatusUnauthorized, "ke.1.e.4",
-		"user_must_be_guest", errors.New("the user must be guest to access this API"))
+	errUserMustBeGuest = kitty.NewError(http.StatusUnauthorized, "ke.us.e.4",
+		"err_user_must_be_guest", errors.New("the user must be guest to access this API"))
 
-	errUserNeedToAuthenticate = kitty.NewError(http.StatusUnauthorized, "ke.1.e.5",
-		"user_must_authenticate", errors.New("the user need to login to access this API"))
+	errUserNeedToAuthenticate = kitty.NewError(http.StatusUnauthorized, "ke.us.e.5",
+		"err_user_must_authenticate", errors.New("the user need to login to access this API"))
 
 	//--------------------------------
 	// Request errors
 	//--------------------------------
-	errRequestIdNotFound = kitty.NewError(http.StatusInternalServerError, "ke.2.e.2",
+	errRequestIdNotFound = kitty.NewError(http.StatusInternalServerError, "ke.rq.e.2",
 		kitty.ErrKeyInternalError, errors.New("request id not found in the request"),
 	)
 
-	errCorrelationIDNotFound = kitty.NewError(http.StatusInternalServerError, "ke.2.e.3",
+	errCorrelationIDNotFound = kitty.NewError(http.StatusInternalServerError, "ke.rq.e.3",
 		kitty.ErrKeyInternalError, errors.New("correlation id not found in the request"),
 	)
 
 	//--------------------------------
 	// DEBUG
 	//--------------------------------
-	errRouteAvaialbeInDebugMode = kitty.NewError(http.StatusUnauthorized, "ke.3.e.0",
-		"route_available_in_debug_mode", errors.New("route is available just in debug mode"),
+	errRouteAvaialbeInDebugMode = kitty.NewError(http.StatusUnauthorized, "ke.dbg.e.0",
+		"err_route_available_in_debug_mode", errors.New("route is available just in debug mode"),
 	)
 
 	//--------------------------------
 	// Other errors
 	//--------------------------------
 	// Set this error status manually on return relative to echo error code.
-	errEchoHTTPError = kitty.NewError(http.StatusNotFound, "ke.4.e.0", kitty.TranslateKeyEmptyMessage, nil)
+	errEchoHTTPError = kitty.NewError(http.StatusNotFound, "ke.ot.e.0", kitty.TranslateKeyEmptyMessage, nil)
 
-	errUnknownError = kitty.NewError(http.StatusInternalServerError, "ke.4.e.1", "err_unknown_error", nil)
+	errUnknownError = kitty.NewError(http.StatusInternalServerError, "ke.ot.e.1", "err_unknown_error", nil)
 )
