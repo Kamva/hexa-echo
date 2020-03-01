b19dc944083f057bd33ad4810e7fb2db4f5da039
Mehran Prs <mehr.prs@gmail.com>
2020-03-01 16:36:23 +0330

    Update ResourceAPI function

--- resource.go
+++ resource.go
@@ -1,6 +1,7 @@
 package kecho
 
 import (
+	"fmt"
 	"github.com/Kamva/kitty"
 	"github.com/labstack/echo/v4"
 )
@@ -33,28 +34,31 @@ type (
 // Ctx method extract the kitty context from the echo context.
 func (r Resource) Ctx(c echo.Context) kitty.Context {
 	return c.Get(ContextKeyKittyCtx).(kitty.Context)
-
 }
 
 // Resource define each method that exists in provided resource.
-func ResourceAPI(group *echo.Group, resource interface{}, m ...echo.MiddlewareFunc) {
+func ResourceAPI(group *echo.Group, resource interface{}, prefix string, m ...echo.MiddlewareFunc) {
 	if r, ok := resource.(GetResource); ok {
-		group.GET("/:id", r.Get, m...)
+		group.GET("/:id", r.Get, m...).Name = routeName(prefix, "get")
 	}
 
 	if r, ok := resource.(CreateResource); ok {
-		group.POST("/:id", r.Create, m...)
+		group.POST("/:id", r.Create, m...).Name = routeName(prefix, "post")
 	}
 
 	if r, ok := resource.(UpdateResource); ok {
-		group.PUT("/:id", r.Update, m...)
+		group.PUT("/:id", r.Update, m...).Name = routeName(prefix, "put")
 	}
 
 	if r, ok := resource.(PatchResource); ok {
-		group.PATCH("/:id", r.Patch, m...)
+		group.PATCH("/:id", r.Patch, m...).Name = routeName(prefix, "patch")
 	}
 
 	if r, ok := resource.(DeleteResource); ok {
-		group.DELETE("/:id", r.Delete, m...)
+		group.DELETE("/:id", r.Delete, m...).Name = routeName(prefix, "delete")
 	}
+}
+
+func routeName(prefix, name string) string {
+	return fmt.Sprintf("%s::%s", prefix, name)
 }
