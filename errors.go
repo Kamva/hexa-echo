da8bb93bdb0e1e3a3adf9b40d4f3bc50a024c531
Mehran Prs <mehr.prs@gmail.com>
2020-03-08 15:45:55 +0330

    Update errors

--- errors.go
+++ errors.go
@@ -8,7 +8,7 @@ import (
 
 var (
 	// Error code description:
-	// KE = hexa echo (package or project name)
+	// hec = hexa echo (package or project name)
 	// 1 = errors about user section (identify some part in application)
 	// E = Error (type of code : error|response)
 	// 00 = error number zero (id of code in that part and type)
@@ -16,47 +16,47 @@ var (
 	//--------------------------------
 	// User and authentication Errors
 	//--------------------------------
-	errUserNotFound = hexa.NewError(http.StatusInternalServerError, "ke.ua.e.0",
+	errUserNotFound = hexa.NewError(http.StatusInternalServerError, "hec.ua.e.0",
 		hexa.ErrKeyInternalError, errors.New("user not found"),
 	)
 
-	errContextUserNotImplementedHexaUser = hexa.NewError(http.StatusInternalServerError, "ke.ua.e.1",
+	errContextUserNotImplementedHexaUser = hexa.NewError(http.StatusInternalServerError, "hec.ua.e.1",
 		hexa.ErrKeyInternalError, errors.New("context's user not implemented the hexa User interface"),
 	)
 
-	errJwtMissing = hexa.NewError(http.StatusBadRequest, "ke.ua.e.2",
+	errJwtMissing = hexa.NewError(http.StatusBadRequest, "hec.ua.e.2",
 		"err_missing_jwt_token", errors.New("missing or malformed jwt"))
 
-	errInvalidOrExpiredJwt = hexa.NewError(http.StatusUnauthorized, "ke.ua.e.3",
+	errInvalidOrExpiredJwt = hexa.NewError(http.StatusUnauthorized, "hec.ua.e.3",
 		"err_invalid_or_expired_jwt", errors.New("invalid or expired jwt"))
 
-	errUserMustBeGuest = hexa.NewError(http.StatusUnauthorized, "ke.ua.e.4",
+	errUserMustBeGuest = hexa.NewError(http.StatusUnauthorized, "hec.ua.e.4",
 		"err_user_must_be_guest", errors.New("the user must be guest to access this API"))
 
-	errUserNeedToAuthenticate = hexa.NewError(http.StatusUnauthorized, "ke.ua.e.5",
+	errUserNeedToAuthenticate = hexa.NewError(http.StatusUnauthorized, "hec.ua.e.5",
 		"err_user_must_authenticate", errors.New("the user need to login to access this API"))
 
-	errRefreshTokenCanNotBeEmpty = hexa.NewError(http.StatusBadRequest, "ke.ua.e.6",
+	errRefreshTokenCanNotBeEmpty = hexa.NewError(http.StatusBadRequest, "hec.ua.e.6",
 		"err_refresh_token_is_empty", errors.New("refresh token can not be empty"))
 
-	errInvalidRefreshToken = hexa.NewError(http.StatusBadRequest, "ke.ua.e.7",
+	errInvalidRefreshToken = hexa.NewError(http.StatusBadRequest, "hec.ua.e.7",
 		"err_invalid_refresh_token", nil)
 
 	//--------------------------------
 	// Request errors
 	//--------------------------------
-	errRequestIdNotFound = hexa.NewError(http.StatusInternalServerError, "ke.rq.e.2",
+	errRequestIdNotFound = hexa.NewError(http.StatusInternalServerError, "hec.rq.e.2",
 		hexa.ErrKeyInternalError, errors.New("request id not found in the request"),
 	)
 
-	errCorrelationIDNotFound = hexa.NewError(http.StatusInternalServerError, "ke.rq.e.3",
+	errCorrelationIDNotFound = hexa.NewError(http.StatusInternalServerError, "hec.rq.e.3",
 		hexa.ErrKeyInternalError, errors.New("correlation id not found in the request"),
 	)
 
 	//--------------------------------
 	// DEBUG
 	//--------------------------------
-	errRouteAvaialbeInDebugMode = hexa.NewError(http.StatusUnauthorized, "ke.dbg.e.0",
+	errRouteAvaialbeInDebugMode = hexa.NewError(http.StatusUnauthorized, "hec.dbg.e.0",
 		"err_route_available_in_debug_mode", errors.New("route is available just in debug mode"),
 	)
 
@@ -64,7 +64,7 @@ var (
 	// Other errors
 	//--------------------------------
 	// Set this error status manually on return relative to echo error code.
-	errEchoHTTPError = hexa.NewError(http.StatusNotFound, "ke.ot.e.0", hexa.TranslateKeyEmptyMessage, nil)
+	errEchoHTTPError = hexa.NewError(http.StatusNotFound, "hec.ot.e.0", hexa.TranslateKeyEmptyMessage, nil)
 
-	errUnknownError = hexa.NewError(http.StatusInternalServerError, "ke.ot.e.1", "err_unknown_error", nil)
+	errUnknownError = hexa.NewError(http.StatusInternalServerError, "hec.ot.e.1", "err_unknown_error", nil)
 )
