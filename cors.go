e146dbda3f2e8eeea75c37f69b6bea3e1d295671
Mehran Prs <mehr.prs@gmail.com>
2020-03-17 11:34:51 +0330

    Add CORS HEADERS

---
+++ cors.go
@@ -0,0 +1,26 @@
+package hecho
+
+import (
+	"github.com/Kamva/hexa"
+	"github.com/labstack/echo/v4/middleware"
+	"net/http"
+)
+
+// CORSConfig returns te CORS config.
+func CorsConfig(config hexa.Config) middleware.CORSConfig {
+	origins := config.GetList("CORS_ALLOW_ORIGINS")
+	methods := config.GetList("CORS_ALLOW_METHODS")
+	headers := config.GetList("CORS_ALLOW_HEADERS")
+	if len(origins) == 0 {
+		origins = []string{"*"}
+	}
+	if len(methods) == 0 {
+		methods = []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete}
+	}
+
+	return middleware.CORSConfig{
+		AllowOrigins: origins,
+		AllowMethods: methods,
+		AllowHeaders: headers,
+	}
+}
