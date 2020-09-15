39b7c4beb017c08dc1a1445bce538e1b0dfa0b7f
Mehran Prs <mehr.prs@gmail.com>
2020-09-15 08:12:49 +0430

    Add template constructor

--- template.go
+++ template.go
@@ -14,3 +14,12 @@ type Template struct {
 func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
 	return t.templates.ExecuteTemplate(w, name, data)
 }
+
+// NewTemplate creaets new template engine.
+// This engine parses the template definitions from
+// the files identified by the pattern.
+func NewTemplate(pattern string) echo.Renderer {
+	return &Template{
+		templates: template.Must(template.ParseGlob(pattern)),
+	}
+}
