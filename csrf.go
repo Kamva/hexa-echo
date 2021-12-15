c7e41fdedac2a20765b35ebeb337984e7416dc6e
mehran-prs <mehr.prs@gmail.com>
2021-12-15 17:17:54 +0330

    Update skipper name

--- csrf.go
+++ csrf.go
@@ -5,12 +5,12 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
-// CSRFSkipper skips if request doesn't need to csrf check.
+// CSRFSkipperByAuthTokenLocation skips if request doesn't need to csrf check.
 // We do csrf check when user's token is in the cookie or session.
 // and the request method is post too.
-func CSRFSkipper(ctx echo.Context) bool {
+func CSRFSkipperByAuthTokenLocation(ctx echo.Context) bool {
 	l := ctx.Get(AuthTokenLocationContextKey)
 	return l != TokenLocationCookie && l != TokenLocationSession
 }
 
-var _ middleware.Skipper = CSRFSkipper
+var _ middleware.Skipper = CSRFSkipperByAuthTokenLocation
